450b79f7bda33a87ccef1624d9694efe013e889d
benyamynbrkyc <benjamin.brkic@gmail.com>
2021-01-05 02:38:15 +0100

    fix(cors): cors

--- src/index.js
+++ src/index.js
@@ -33,8 +33,8 @@ db.once('open', () => {
 
 const app = express();
 app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Origin', '*');
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
